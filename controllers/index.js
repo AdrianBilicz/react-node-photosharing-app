914423230302f357426946140d8a6c0345a3d29c
kmicic91 <bilicz.adrian@gmail.com>
2017-09-27 14:30:33 -0400

    api

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+var ProfileController = require ('./ProfileController')
+var PostController = require ('./PostController')
+var CommentController = require ('./CommentController')
+
+module.exports = {
+	profile: ProfileController,
+	post: PostController,
+	comment: CommentController
+}
